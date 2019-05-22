d67dcb76fb1db3b8c737e78fba2bc82fd5d65c2e
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2019-05-22 12:26:06 -0700

    [brief] Adds install script for Windows.
    [detailed]
    - Easier than having to type stuff by hand.

---
+++ install.bat
@@ -0,0 +1,37 @@
+@echo off
+
+:: First check if we're running as admin.
+echo Install script must be run as root. Checking permissions...
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    echo Root detected.
+    echo Beginning install...
+) else (
+    echo Error: script must be run as root. Exiting...
+    exit /B
+)
+
+:: Grab the current directory.
+set CURDIR=%~dp0
+
+:: Set the Vim environment variable.
+setx -m VIM "C:\Vim"
+
+:: First setup the vim shortcuts.
+mklink %VIM%\_vimrc %CURDIR%nvim\init.vim
+mklink %VIM%\_gvimrc %CURDIR%nvim\gvim.vim
+mklink /D %VIM%\autoload %CURDIR%nvim\autoload
+mklink /D %VIM%\colors %CURDIR%nvim\colors
+mklink /D %VIM%\ftplugin %CURDIR%nvim\ftplugin
+mklink /D %VIM%\plugin %CURDIR%nvim\plugin
+mklink /D %VIM%\test-files %CURDIR%nvim\test-files
+
+:: Now setup the git stuff
+mklink %USERPROFILE%\.gitconfig %CURDIR%git\config
+mklink %USERPROFILE%\.gitignore %CURDIR%git\ignore
+mklink %USERPROFILE%\.gittemplate.txt %CURDIR%git\template
+
+:: Finally, setup the bashrc.
+mklink %USERPROFILE%\.bashrc %CURDIR%bash\bashrc
+
+echo Install finished successfully
