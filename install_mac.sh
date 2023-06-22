0d5b52d86d488e9238b5790a58dac6f83ed5b1c4
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2023-06-22 15:51:18 -0700

    [brief] Adds special zsh files for mac.
    [detailed]
    - Mostly to fix the issues that arise with it.
    - Also maps gvim to vimr for a nice GUI.

---
+++ install_mac.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#vim:set et sw=2 ts=4 tw=84:
+
+ln -s "$PWD/zsh/zshrc_mac" "$HOME/.zshrc"
+ln -s "$PWD/zsh/zsh_plugins" "$HOME/.zsh_plugins.txt"
+ln -s "$PWD/zsh/p10k.zsh" "$HOME/.p10k.zsh"
+ln -s "$PWD/zsh/zlogin" "$HOME/.zlogin"
+ln -s "$PWD/git/config_linux" "$HOME/.gitconfig"
+ln -s "$PWD/git/ignore" "$HOME/.gitignore"
+ln -s "$PWD/git/template" "$HOME/.gittemplate.txt"
+ln -s "$PWD/nvim" "$HOME/.config/nvim"
+ln -s "$PWD/cpp/.clang-format" "$HOME/.clang-format"
+ln -s "$PWD/python/pylintrc" "$HOME/pylintrc"
+ln -s "$PWD/tmux/tmux.conf" "$HOME/.tmux.conf"
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
