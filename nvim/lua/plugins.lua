0eefee4bd6cb2b8be0a3c65a474ac7dd2db272cb
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2023-07-17 15:31:25 -0700

    [brief] Fixes a typo for disabling mccabe.
    [detailed]

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -29,7 +29,7 @@ lspconfig.pylsp.setup({
             plugins = {
                 autopep8 = {enabled = false},
                 flake8 = {enabled = false},
-                mccabe = {eanbled = false},
+                mccabe = {enabled = false},
                 pycodestyle = {enabled = false},
                 pydocstyle = {enabled = false},
                 pyflakes = {enabled = false},
