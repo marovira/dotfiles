c3756066c383e87eada3dbcc27d9e364dbdf0f45
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2023-07-26 15:24:35 -0700

    [brief] LSP sources are now set depending on context.
    [detailed]
    - This allows us to have spelling autocomplete when we want (comments,
      text files, etc) and LSP when we're writing actual code.
    - Also ensures that pandoc gets spelling.

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -4,10 +4,6 @@ return {
         cmd = "LspInfo",
         event = { "BufReadPre", "BufNewFile" },
         dependencies = {
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "hrsh7th/cmp-nvim-lsp-signature-help" },
-            { "hrsh7th/cmp-path" },
-            { "hrsh7th/cmp-buffer" },
             { "williamboman/mason-lspconfig.nvim" },
             {
                 "williamboman/mason.nvim",
--- nvim/lua/plugins/nvim_cmp.lua
+++ nvim/lua/plugins/nvim_cmp.lua
@@ -4,6 +4,11 @@ return {
         event = "InsertEnter",
         dependencies = {
             { "L3MON4D3/LuaSnip" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lsp-signature-help" },
+            { "hrsh7th/cmp-path" },
+            { "hrsh7th/cmp-buffer" },
+            { "f3fora/cmp-spell" },
         },
         config = function()
             require("lsp-zero.cmp").extend({
@@ -17,10 +22,42 @@ return {
 
             local cmp = require("cmp")
             local cmp_action = require("lsp-zero").cmp_action()
+            local context = require("cmp.config.context")
             cmp.setup({
                 mapping = {
                     ["<Tab>"] = cmp_action.tab_complete(),
                     ["<S-Tab>"] = cmp_action.select_prev_or_fallback(),
+                },
+                sources = {
+                    {
+                        name = "spell",
+                        option = {
+                            enable_in_context = function()
+                                return context.in_treesitter_capture("spell")
+                                    or vim.bo.filetype == "pandoc"
+                            end,
+                        },
+                    },
+                    { name = "path" },
+                    { name = "buffer", keyword_length = 3 },
+                    {
+                        name = "nvim_lsp",
+                        option = {
+                            enable_in_context = function()
+                                return context.in_treesitter_capture("comment")
+                                    or context.in_syntax_group("Comment")
+                            end,
+                        },
+                    },
+                    {
+                        name = "nvim_lsp_signature_help",
+                        option = {
+                            enable_in_context = function()
+                                return context.in_treesitter_capture("comment")
+                                    or context.in_syntax_group("Comment")
+                            end,
+                        },
+                    },
                 },
             })
         end,
