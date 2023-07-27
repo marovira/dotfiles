6eee852f4e169f7c710267b7092ba706200a37bc
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2023-07-27 14:23:43 -0700

    [brief] Removes the buffer navigation maps.
    [detailed]
    - It seems that they either "just work" in neovim, or the tmux plugin is
      handling it for us. Either way, things work without them.

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -23,11 +23,5 @@ end)
 -- Clear search highlight
 vim.keymap.set("n", "<ESC>", ":noh<CR><ESC>", { silent = true })
 
--- Buffer navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-
 -- Play macro stored in buffer q with the space bar.
 vim.keymap.set("n", "<Space>", "@q")
