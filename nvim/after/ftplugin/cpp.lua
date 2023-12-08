9c50bbded9aba1139dcfa3de1d935b38076f266d
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2023-12-08 11:01:32 -0800

    [brief] Set the correct executable for clang-format on Linux.
    [detailed]

--- nvim/after/ftplugin/cpp.lua
+++ nvim/after/ftplugin/cpp.lua
@@ -1,3 +1,6 @@
+package.path = package.path .. ";../../lua;"
+local common = require("common")
+
 -- Single-compile
 vim.keymap.set("n", "<F9>", ":SCCompile<CR>")
 vim.keymap.set("n", "<F10>", ":SCCompileRun<CR>")
@@ -7,3 +10,11 @@ vim.keymap.set("n", "<F10>", ":SCCompileRun<CR>")
 vim.b.ale_linters = nil
 vim.b.ale_fixers = { "clang-format" }
 vim.b.ale_linters_explicit = true
+
+-- Linux naming conventions for clang are different than on Windows (shocking) so let's
+-- set them up correctly. Assume Clang 15.
+if common.is_windows() then
+    vim.b.ale_c_clangformat_executable = "clang-format"
+else
+    vim.b.ale_c_clangformat_executable = "clang-format-15"
+end
