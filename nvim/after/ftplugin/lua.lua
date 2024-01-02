db9bd8cd9f9e33c793c4dd627b46825ea029bbda
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2024-01-02 11:03:39 -0800

    [brief] Some updates for ALE fixers.
    [detailed]
    - Adds mypy to Python. I'm going to try it out for a bit and see how it
      feels. If it doesn't get too much in the way of things and actually
      helps, then I'll keep it.
    - When formatting with stylua, make sure the parent directories are
      searched.

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -2,3 +2,4 @@
 vim.b.ale_linters = nil
 vim.b.ale_fixers = { "stylua" }
 vim.b.ale_linters_explicit = true
+vim.g.ale_lua_stylua_options = "--search-parent-directories"
--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,5 +1,5 @@
 -- Enable linting with pylint and formatting with black.
-vim.b.ale_linters = { "pylint" }
+vim.b.ale_linters = { "pylint", "mypy" }
 vim.b.ale_fixers = { "black" }
 vim.b.ale_linters_explicit = true
 vim.b.ale_lint_on_text_changed = false
