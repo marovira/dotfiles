483d4008ecb61df895d9b87a665a22e703da7091
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2024-03-23 22:12:20 -0700

    [brief] Adds Ruff as a linter/fixer for python files.
    [detailed]
    - This is mostly a transition thing while I move stuff away from
      pylint/black. Eventually I'll get rid of those two and just have
      everything done through Ruff.
    - I also need to add the sample config for Ruff once I figure out what I
      want.

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,7 +1,7 @@
--- Enable linting with pylint and formatting with black.
-vim.b.ale_linters = { "pylint", "mypy" }
-vim.b.ale_fixers = { "black" }
+vim.b.ale_linters = { "ruff", "mypy", "pylint" }
+vim.b.ale_fixers = { "ruff", "ruff_format", "black" }
 vim.b.ale_linters_explicit = true
 vim.b.ale_lint_on_text_changed = false
+vim.b.ale_lint_on_insert_leave = true
 vim.b.ale_lint_on_save = true
 vim.b.ale_lint_on_enter = true
