0cb2518a3e15c4d04bcdee06e4c729b21514020c
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2024-08-28 17:09:57 -0700

    [brief] Add clangd as a linter for C++.
    [detailed]
    - After much work I've finally figured out how to get clang to play nice
      on Windows.

--- nvim/after/ftplugin/cpp.lua
+++ nvim/after/ftplugin/cpp.lua
@@ -3,7 +3,7 @@ local common = require("common")
 
 -- No linting (I would have to figure out which settings I want for a clang-tidy file, and
 -- I honestly don't think it's worth it), but keep formatting.
-vim.b.ale_linters = nil
+vim.b.ale_linters = { "clangd" }
 vim.b.ale_fixers = { "clang-format" }
 vim.b.ale_linters_explicit = true
 
