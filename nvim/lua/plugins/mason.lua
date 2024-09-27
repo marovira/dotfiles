bd71e0c33e11f89094eb035dcf46082c502d9116
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2024-09-26 22:39:05 -0700

    [brief] Adds DAP.
    [detailed]
    - The current setup for Python is fully functional, though I will need
      to re-think the bindings for running pytest functions. I'll do that
      when I'm not so tired.
    - The setup for C++ works for single files. I need to figure out how to
      make it work for CMake projects.

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,5 +1,47 @@
-local build = function()
+local mason_build = function()
     pcall(vim.api.nvim_command, "MasonUpdate")
+end
+
+local mason_dap_config = function()
+    require("mason").setup()
+    require("mason-nvim-dap").setup({
+        automatic_setup = true,
+        ensure_installed = {
+            "python",
+            "codelldb",
+        },
+        handlers = {
+            function(config)
+                require("mason-nvim-dap").default_setup(config)
+            end,
+        },
+        cpp = function(config)
+            config.adapters = {
+                type = "server",
+                port = "${port}",
+                executable = {
+                    command = vim.fs.joinpath(
+                        ---@diagnostic disable-next-line: param-type-mismatch
+                        vim.fn.stdpath("data"),
+                        "mason",
+                        "bin",
+                        "codelldb"
+                    ),
+                    args = { "--port", "${port}" },
+                },
+            }
+            config.configurations = {
+                name = "Launch file",
+                type = "codelldb",
+                request = "launch",
+                program = "${fileDirname}/" .. "${fileBasenameNoExtension}",
+                cwd = "${fileDirname}",
+                stopOnEntry = false,
+                args = {},
+            }
+            require("mason-nvim-dap").default_setup(config)
+        end,
+    })
 end
 
 return {
@@ -7,6 +49,15 @@ return {
         "williamboman/mason.nvim",
         lazy = false,
         config = true,
-        build = build,
+        build = mason_build,
+    },
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        dependencies = {
+            { "williamboman/mason.nvim" },
+            { "mfussenegger/nvim-dap" },
+        },
+        lazy = false,
+        config = mason_dap_config,
     },
 }
