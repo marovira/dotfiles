903997dcb44e6e65d9b3aeab305b8f02c49c90f1
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2025-03-09 21:16:53 -0700

    [brief] Add hardtime.
    [detailed]
    - Time to kick off more bad habits!

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -197,4 +197,25 @@ return {
             },
         },
     },
+    {
+        "m4xshen/hardtime.nvim",
+        dependencies = { "MunifTanjim/nui.nvim" },
+        opts = {
+            disabled_filetype = { "startify" },
+            hints = {
+                ["[dcyvV][ia][%(%)]"] = {
+                    message = function(keys)
+                        return "Use " .. keys:sub(1, 2) .. "b instead of " .. keys
+                    end,
+                    length = 3,
+                },
+                ["[dcyvV][ia][%{%}]"] = {
+                    message = function(keys)
+                        return "Use " .. keys:sub(1, 2) .. "B instead of " .. keys
+                    end,
+                    length = 3,
+                },
+            },
+        },
+    },
 }
