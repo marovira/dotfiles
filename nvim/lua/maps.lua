7007fe9e41b78bee05d44869bb590c706e05da11
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2025-04-20 08:55:51 -0700

    [brief] Few QOL improvements for jumps in vim.
    [detailed]

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -41,3 +41,34 @@ vim.keymap.set(
     "<cmd>lua vim.diagnostic.goto_next()<cr>",
     { desc = "LSP go to next diagnostic" }
 )
+
+-- Movement keymaps
+-- Allow returning to starting position when doing relative vertical jumps with C-o.
+vim.keymap.set("n", "j", function()
+    if vim.v.count > 0 then return "m'" .. vim.v.count .. "j" end
+    return "j"
+end, { expr = true })
+vim.keymap.set("n", "k", function()
+    if vim.v.count > 0 then return "m'" .. vim.v.count .. "k" end
+    return "k"
+end, { expr = true })
+
+-- Return to the start of search.
+local mark_search_keys = {
+    ["/"] = "Search forward",
+    ["?"] = "Search backward",
+    ["*"] = "Search current word (forward)",
+    ["#"] = "Search current word (backward)",
+    ["g*"] = "Search current word (forward, not whole word)",
+    ["g#"] = "Search current word (backward, not whole word)",
+}
+
+for key, desc in pairs(mark_search_keys) do
+    vim.keymap.set("n", key, "ms" .. key, { desc = desc })
+end
+
+-- Clear search highlight when jumping back tot he beginning.
+vim.keymap.set("n", "`s", function()
+    vim.cmd("normal! `s")
+    vim.cmd.nohlsearch()
+end)
