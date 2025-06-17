267056cfc3d37e1efc87bfc0630ab6ff691ca5b3
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2025-06-17 12:41:06 -0700

    [brief] Fixes transparency issues with neovide.
    [detailed]

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -5,7 +5,7 @@ return {
         priority = 1000,
         opts = {
             style = "moon",
-            transparent = true,
+            transparent = not vim.g.neovide,
             styles = {
                 floats = "transparent",
                 sidebars = "transparent",
