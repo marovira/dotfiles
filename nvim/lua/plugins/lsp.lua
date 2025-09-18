b1fd5a65e6a8750449abefb28191aaee523a068c
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2025-09-18 11:32:39 -0700

    [brief] Add a plugin for wezterm LSP support.
    [detailed]
    - Need to go through and add type-annotations, but it's good to have.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -66,10 +66,14 @@ return {
     },
     {
         "folke/lazydev.nvim",
+        dependencies = {
+            { "DrKJeff16/wezterm-types", lazy = true },
+        },
         ft = "lua",
         opts = {
             library = {
                 { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                { path = "wezterm-types", mods = { "wezterm" } },
             },
         },
     },
