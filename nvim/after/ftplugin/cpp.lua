e6b0ea8e5c4dfda4e8db062bc6c1e9894fa596d5
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2025-10-16 14:13:07 -0700

    [brief] Better detection for clang tooling on Linux.
    [detailed]
    - This automatically detects the latest version installed of clang.
      Maybe in the future we should have a way of overriding this?

--- nvim/after/ftplugin/cpp.lua
+++ nvim/after/ftplugin/cpp.lua
@@ -1,6 +1,12 @@
 package.path = package.path .. ";../../lua;"
 local common = require("common")
 
+-- List of supported clang versions. As newer versions are tested, add them here.
+local llvm_versions = { "18", "19", "20", "21" }
+
+vim.g.ale_cpp_cc_options = "-std=c++20 -Wall --pedantic"
+vim.g.ale_cpp_clangd_options = "--header-insertion=never"
+
 vim.b.ale_linters = { "clangtidy" }
 vim.b.ale_fixers = { "clang-format" }
 vim.b.ale_linters_explicit = true
@@ -9,18 +15,26 @@ vim.b.ale_lint_on_insert_leave = true
 vim.b.ale_lint_on_save = true
 vim.b.ale_lint_on_enter = true
 
--- Linux naming conventions for clang are different than on Windows (shocking) so let's
--- set them up correctly. Assume Clang 18.
 if common.is_windows() or common.is_mac() then
-    vim.b.ale_c_clangformat_executable = "clang-format"
     vim.b.ale_cpp_cc_executable = "clang++"
+    vim.b.ale_cpp_clangformat_executable = "clang-format"
+    vim.b.ale_cpp_clangtidy_executable = "clang-tidy"
 else
-    vim.b.ale_c_clangformat_executable = "clang-format-18"
-    vim.b.ale_cpp_cc_executable = "clang++-18"
+    local clang = "clang++-"
+    local fmt = "clang-format-"
+    local tidy = "clang-tidy-"
+    for _, ver in ipairs(llvm_versions) do
+        if vim.fn.executable(clang .. ver) == 1 then
+            vim.b.ale_cpp_cc_executable = clang .. ver
+        end
+        if vim.fn.executable(fmt .. ver) == 1 then
+            vim.b.ale_cpp_clangformat_executable = fmt .. ver
+        end
+        if vim.fn.executable(tidy .. ver) == 1 then
+            vim.b.ale_cpp_clangtidy_executable = tidy .. ver
+        end
+    end
 end
-
-vim.g.ale_cpp_cc_options = "-std=c++20 -Wall --pedantic"
-vim.g.ale_cpp_clangd_options = "--header-insertion=never"
 
 if common.is_windows() then
     vim.opt_local.makeprg =
