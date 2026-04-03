c08ac86512eceefdbbb92306a91520043173ad76
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2026-04-02 17:10:12 -0700

    nvim: Add sidekick for AI

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -630,4 +630,23 @@ return {
             },
         },
     },
+    {
+        "folke/sidekick.nvim",
+        keys = {
+            {
+                "<leader>ac",
+                function()
+                    require("sidekick.cli").toggle({ name = "claude", focus = true })
+                end,
+                desc = "Sidekick toggle Claude",
+            },
+            {
+                "<leader>ag",
+                function()
+                    require("sidekick.cli").toggle({ name = "gemini", focus = true })
+                end,
+                desc = "Sidekick toggle Gemini",
+            },
+        },
+    },
 }
