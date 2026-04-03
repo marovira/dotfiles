9e142eb5e256a95a0ba775edb348c9ef2ae95ba1
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2026-04-03 13:18:42 -0700

    git: Improve the install_pacman script
    This covers the following issues:
    1. Better handling of the version mismatch that sometimes occurs between
       git-sdk-64 and the version of git that is shipped. We now let pacman
       figure out what version to use.
    2. Remove the secondary signing key from GPG. The key is not really
       needed and just adds unnecessary noise.

--- git/install_pacman.sh
+++ git/install_pacman.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Script to install pacman package management
 # in Git for Windows Portable
 # To be run from a git-bash session.
@@ -9,12 +11,12 @@ RAWURLB="https://raw.githubusercontent.com/git-for-windows/git-sdk-64"
 rm -rf git-sdk-64
 # Clone tiny blobless shallow repo
 git clone \
-	--depth 150 \
-	--filter=blob:none \
-	--no-checkout \
-	$GITURL
+    --depth 150 \
+    --filter=blob:none \
+    --no-checkout \
+    $GITURL
 
-cd  git-sdk-64
+cd git-sdk-64 || exit
 git pull
 
 ### Pre install minimal pacman bootstrap
@@ -22,86 +24,73 @@ d="var/lib/pacman/local"
 mkdir -p "/$d"
 pkgs=('pacman-[0-9]' 'gettext-[0-9]' 'pacman-mirrors-' 'msys2-keyring-')
 for j in ${pkgs[@]} ; do
-	pacvers=$(basename $( git show main:$d|grep "$j" ))
-	echo $pacvers
-	shfiles=$(curl -sSL $RAWURLB/main/$d/$pacvers/files )
-	for f in $shfiles
-	do
-	 if [[ $f = *"/"* ]] && [[ $f != *"/man/"* ]] && [[ $f != *"locale/"* ]] && [[ $f != *\/ ]]
-	  then
-	  		if [ ! -f "/$f" ]
-				then
-					mkdir -p /$(dirname "$f" )
-					curl -sSL $RAWURLB/main/$f -o /$f &
-					[ $( jobs | wc -l ) -ge $( nproc ) ] && wait
-			fi
-	 fi
-	done
-	wait
+    pacvers=$(basename $( git show main:$d | grep "$j" ))
+    echo "Bootstrapping: $pacvers"
+    shfiles=$(curl -sSL $RAWURLB/main/$d/$pacvers/files )
+    for f in $shfiles
+    do
+        if [[ $f = *"/"* ]] && [[ $f != *"/man/"* ]] && [[ $f != *"locale/"* ]] && [[ $f != *\/ ]]
+        then
+            if [ ! -f "/$f" ]; then
+                mkdir -p /$(dirname "$f" )
+                curl -sSL $RAWURLB/main/$f -o /$f &
+                [ $( jobs | wc -l ) -ge $( nproc ) ] && wait
+            fi
+        fi
+    done
+    wait
 done
 wait
 
 mkdir -p /var/lib/pacman
+rm -rf /etc/pacman.d/gnupg
 pacman-key --init
 pacman-key --populate msys2
 curl -L https://raw.githubusercontent.com/git-for-windows/build-extra/HEAD/git-for-windows-keyring/git-for-windows.gpg |\
 pacman-key --add - &&
-pacman-key --lsign-key E8325679DFFF09668AD8D7B67115A57376871B1C &&
-pacman-key --lsign-key 3B6D86A1BA7701CD0F23AED888138B9E1A9F3986
+pacman-key --lsign-key E8325679DFFF09668AD8D7B67115A57376871B1C
 pacman -Sy
 
 ################
 #Restore Pacman metadata
-d="var/lib/pacman/local"
-for j in ${pkgs[@]} ; do
-	pacvers=$(basename $( git show main:$d|grep "$j" ))
-	for f in desc install files mtree; do
-		git show main:$d/$pacvers/$f > /dev/null 2>%1
-		if [ $? -eq 0 ]; then
-		mkdir -p /$d/$pacvers
-		[ ! -f "/$d/$pacvers/$f" ] && curl -sSL "$RAWURL/main/$d/$pacvers/$f" -o /$d/$pacvers/$f
-		echo "$d/$pacvers/$f"
-		fi
-	done
-done
-#################
 commits=$(git log --pretty=%H)
-######## Packages metadata
+
 spdup ()
 {
-	package=$1
-	version=$2
-	for cs in $commits ; do
-	    d=var/lib/pacman/local/$package-$version
-		[ ! -d /$d ] && mkdir -p /$d
-		git show $cs:$d >/dev/null 2>&1
-		if [ $? -eq 0 ]
-		then
-			for f in desc files install mtree; do
-				git show $cs:$d/$f > /dev/null 2>&1
-				if [ $? -eq 0 ]
-				then
-				 [ ! -f "/$d/$f" ] && curl -sSL "$RAWURL/$cs/$d/$f" -o /$d/$f
-				fi
-			done
-			echo -e "$cs\t$package $version"
-			break
-		fi
-	done
+    package=$1
+    version=$2
+    found=false
+    for cs in $commits ; do
+        d=var/lib/pacman/local/$package-$version
+        if git show $cs:$d >/dev/null 2>&1; then
+            [ ! -d /$d ] && mkdir -p /$d
+            for f in desc files install mtree; do
+                if git show $cs:$d/$f > /dev/null 2>&1; then
+                    [ ! -f "/$d/$f" ] && curl -sSL "$RAWURL/$cs/$d/$f" -o /$d/$f
+                fi
+            done
+            echo -e "Restored:\t$package $version"
+            found=true
+            break
+        fi
+    done
+
+    # If version not found in SDK, clear the local folder so pacman can fresh-install
+    if [ "$found" = false ]; then
+        echo -e "DRIFT DETECTED:\t$package $version (Skipping metadata)"
+        rm -rf /var/lib/pacman/local/$package-$version
+    fi
 }
 
+while read -r package version; do
+    [ -z "$package" ] && continue
+    spdup "$package" "$version"&
+    if [[ $(jobs -r | wc -l) -ge $(nproc) ]]; then
+        wait -n
+    fi
+done < /etc/package-versions.txt
 
-cat /etc/package-versions.txt |while read package version
-do
-	spdup $package $version &
-	[ $( jobs | wc -l ) -ge $( nproc ) ] && wait
-
-done
-wait
-
-### Wrap up
-
-pacman -Sy filesystem libxml2 liblzma icu gcc-libs bash-completion --noconfirm
+pacman -Sy --noconfirm --overwrite '*' filesystem libxml2 liblzma icu gcc-libs bash-completion
 
 # Sync new repositories. This will also allow downgrades as well. This will also kill the MSYS2
 echo
@@ -119,4 +108,4 @@ echo "###################################################################"
 echo
 echo
 read -rs -p $"Press escape or arrow key to continue or wait 5 seconds..." -t 5 -d $'\e';echo;
-pacman -Syyuu
+pacman -Syyuu --overwrite "*"
