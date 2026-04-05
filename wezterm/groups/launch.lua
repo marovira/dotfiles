842b3ce5ff003caaa8833a2b607206890cbfb6a3
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2026-04-05 12:42:03 -0700

    wez: Use msys2 instead of git bash

--- wezterm/groups/launch.lua
+++ wezterm/groups/launch.lua
@@ -3,10 +3,27 @@ local common = require("utils")
 local cfg = {}
 
 if common.is_windows() then
+    local args = {
+        "cmd.exe",
+        "/c",
+        os.getenv("USERPROFILE") .. "/scoop/apps/msys2/current/msys2_shell.cmd",
+        "-mingw64",
+        "-shell",
+        "zsh",
+        "-defterm",
+        "-no-start",
+        "-use-full-path",
+    }
+    local env = {
+        MSYS2_PATH_TYPE = "inherit",
+        CHERE_INVOKING = "1",
+    }
+
     local launch_menu = {
         {
-            label = "Git Bash",
-            args = { "bash.exe" },
+            label = "MSYS2",
+            args = args,
+            set_environment_variables = env,
         },
         {
             label = "Command Prompt",
@@ -20,7 +37,8 @@ if common.is_windows() then
 
     cfg.launch_menu = launch_menu
 
-    cfg.default_prog = { "bash.exe" }
+    cfg.default_prog = args
+    cfg.set_environment_variables = env
     cfg.default_cwd = common.get_default_cwd()
 end
 
